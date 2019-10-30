379202b7db50df82a1f8fc1776bedd9159766d55
Diana Tsevan <dziyana@form.io>
2019-10-30 12:10:06 +0300

    Added builderComponents template; added grip-gap class for builder template

--- src/templates/uswds/index.ts
+++ src/templates/uswds/index.ts
@@ -1,9 +1,13 @@
 import builder from './builder';
 import builderComponent from './builderComponent';
+import builderComponents from './builderComponents';
+import builderEditForm from './builderEditForm';
 import input from './input';
 
 export default {
   builder,
   builderComponent,
+  builderComponents,
+  builderEditForm,
   input,
 };
