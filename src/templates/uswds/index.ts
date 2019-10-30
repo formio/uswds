6f9e42694f24d0258d1d73b397ed880240afd054
Diana Tsevan <dziyana@form.io>
2019-10-30 12:56:30 +0300

    Added button component template

--- src/templates/uswds/index.ts
+++ src/templates/uswds/index.ts
@@ -2,6 +2,7 @@ import builder from './builder';
 import builderComponent from './builderComponent';
 import builderComponents from './builderComponents';
 import builderEditForm from './builderEditForm';
+import button from './button';
 import input from './input';
 
 export default {
@@ -9,5 +10,6 @@ export default {
   builderComponent,
   builderComponents,
   builderEditForm,
+  button,
   input,
 };
