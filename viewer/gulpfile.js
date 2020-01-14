578b449b22889a8256fcecaeca388098d90f6f4f
Diana Tsevan <dziyana@form.io>
2020-01-14 17:37:32 +0300

    Added gulp tasks to add uswds imgs and fonts to application

--- viewer/gulpfile.js
+++ viewer/gulpfile.js
@@ -5,8 +5,10 @@ gulp.task('html', () => gulp.src('./src/index.html').pipe(gulp.dest('dist')));
 gulp.task('assets', () => gulp.src('./src/assets/**/*').pipe(gulp.dest('dist/assets')));
 gulp.task('formiojs', () => gulp.src('./node_modules/formiojs/dist/**/*').pipe(gulp.dest('dist/lib/formiojs')));
 gulp.task('seamless', () => gulp.src('./node_modules/seamless/build/**/*').pipe(gulp.dest('dist/lib/seamless')));
-gulp.task('font-awesome', () => gulp.src(['./node_modules/font-awesome/css/font-awesome.css', './node_modules/font-awesome/fonts/*']).pipe(gulp.dest('dist/lib/fonts')));
+gulp.task('font-awesome', () => gulp.src(['./node_modules/font-awesome/css/font-awesome.css', './node_modules/font-awesome/fonts/*']).pipe(gulp.dest('dist/lib/fontawesome')));
 gulp.task('uswds-styles', () => gulp.src('./node_modules/uswds/dist/css/uswds.min.css').pipe(gulp.dest('dist/lib/uswds')));
+gulp.task('uswds-img', () => gulp.src('./node_modules/uswds/dist/img/**/*').pipe(gulp.dest('dist/lib/img')));
+gulp.task('uswds-fonts', () => gulp.src('./node_modules/uswds/dist/fonts/**/*').pipe(gulp.dest('dist/lib/fonts')));
 gulp.task('custom', () => gulp.src('./node_modules/@formio/uswds/lib/css/styles.css').pipe(gulp.dest('dist/lib/uswds')));
 gulp.task('build', gulp.series('clean', gulp.parallel(
   'html',
@@ -16,6 +18,8 @@ gulp.task('build', gulp.series('clean', gulp.parallel(
   'custom',
   'font-awesome',
   'uswds-styles',
+  'uswds-img',
+  'uswds-fonts',
 )));
 
 const s3 = require("gulp-s3");
