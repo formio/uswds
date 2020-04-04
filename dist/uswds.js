d3278b451afde2fff8f6c141c443dd17ce1b1450
Travis Tidwell <travis@form.io>
2020-04-03 22:24:43 -0500

    Adding powered by form.io to template.

--- dist/uswds.js
+++ dist/uswds.js
@@ -1216,7 +1216,7 @@ eval("Object.defineProperty(exports, \"__esModule\", {\n  value: true\n});\nexpo
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-eval("Object.defineProperty(exports, \"__esModule\", {\n  value: true\n});\nexports.default=function(ctx) {\nvar __t, __p = '';\n__p += '<div class=\"' +\n((__t = (ctx.classes)) == null ? '' : __t) +\n'\" ref=\"webform\" novalidate>' +\n((__t = (ctx.children)) == null ? '' : __t) +\n'</div>\\n';\nreturn __p\n}\n\n//# sourceURL=webpack://uswds/./lib/templates/uswds/webform/form.ejs.js?");
+eval("Object.defineProperty(exports, \"__esModule\", {\n  value: true\n});\nexports.default=function(ctx) {\nvar __t, __p = '';\n__p += '<div class=\"' +\n((__t = (ctx.classes)) == null ? '' : __t) +\n'\" ref=\"webform\" novalidate>' +\n((__t = (ctx.children)) == null ? '' : __t) +\n'</div>\\n<!-- IMPORTANT NOTICE: To remove this section, you must have an enterprise license, or according to OSL 3.0, you must disclose that you removed it. -->\\n<small style=\"float:right;\">powered by &nbsp;<a target=\"_blank\" href=\"https://form.io\"><img style=\"height:20px;float:right;\" src=\"https://help.form.io/assets/formio-logo.png\" /></a></small>\\n<!-- ---------------------------------------------------------------------------------------------------------------------------------------------- -->\\n';\nreturn __p\n}\n\n//# sourceURL=webpack://uswds/./lib/templates/uswds/webform/form.ejs.js?");
 
 /***/ }),
 
