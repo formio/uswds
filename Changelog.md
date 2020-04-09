85487396db03354091c8cf514db863ce9a80f6ba
Anton K <ntnkonon@gmail.com>
2020-04-09 23:37:06 +0300

    Updated builds and version

--- Changelog.md
+++ Changelog.md
@@ -1,3 +1,9 @@
+## 1.3.2
+### Changed
+ - Added column size variable to columns template.
+ - Added mapping function to map bootstrap columns sizes to uswds.
+ - Fixed margins in component template.
+
 ## 1.3.1
 ### Changed
  - Fixed documentation.
--- viewer/src/renderer.js
+++ viewer/src/renderer.js
@@ -1,15 +1,4 @@
 import { Formio } from 'formiojs/formio.form.js';
-
-/** HACK TO GET VIEWER TO WORK. **/
-const Input = Formio.Components.components.input;
-Input.prototype.isFirefox = function() {
-  return typeof InstallTrigger !== 'undefined';
-};
-
-Input.prototype.isIE = function() {
-  return /*@cc_on!@*/false || !!document.documentMode;
-};
-
 import USWDS from '@formio/uswds';
 import VPAT from '@formio/vpat';
 import Premium from '@formio/premium';
