f24bb277b6dea943e5badf1a68715a977cb078f8
Anton K <ntnkonon@gmail.com>
2020-11-18 05:33:14 +0300

    VPAT-623 Fixed incorrect display of choicesjs selects in some cases

--- src/components/Select.ts
+++ src/components/Select.ts
@@ -5,7 +5,7 @@ const SelectComponent = Components.components.select as any;
 export default class USWDSSelectComponent extends SelectComponent {
   choicesOptions() {
     const choicesOptions = super.choicesOptions();
-    choicesOptions.classNames.containerInner += ' usa-select';
+    choicesOptions.classNames.containerInner += ' usa-select maxw-full';
     return choicesOptions;
   }
 }
