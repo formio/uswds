8e4b0d8fb7010fb49176e67608c9c6dbb53ec66e
Travis Tidwell <travis@form.io>
2021-03-05 10:41:35 -0600

    Updated latest viewer.

--- viewer/src/components/ViewerCalendar.js
+++ viewer/src/components/ViewerCalendar.js
@@ -3,9 +3,14 @@ import { Widgets } from 'formiojs';
 const CalendarWidget = Widgets.widgets.calendar;
 
 export class ViewerCalendar extends CalendarWidget {
+  constructor(settings, component) {
+    super(settings, component);
+    this.originalDisplayInTimezone = this.settings.displayInTimezone;
+  }
+
   get timezone() {
     // Ensure the displayInTimezone is never viewer since the server "views" the submission...
-    if (!this.settings.displayInTimezone || (this.settings.displayInTimezone === 'viewer')) {
+    if (!this.originalDisplayInTimezone || this.originalDisplayInTimezone === 'viewer') {
       this.settings.displayInTimezone = this.settings.submissionTimezone ? 'submission' : 'utc';
     }
     return super.timezone;
