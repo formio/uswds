adb5a5674b01eb4b2fef638b805cff5f1f798930
kcburge-formio <kcburge-formio@users.noreply.github.com>
2026-01-07 15:23:36 +0000

    $'syncing commit from monorepo. PR: 424, Title: format gulp files'

--- gulpfile.js
+++ gulpfile.js
@@ -11,56 +11,57 @@ gulp.task('clean', require('del').bind(null, ['dist', 'lib']));
 
 // Compile all *.ejs files to pre-compiled templates and append *.js to the filename.
 gulp.task('templates', () =>
-  gulp.src('./src/**/*.ejs')
-    .pipe(template.precompile({
-      evaluate: /\{%([\s\S]+?)%\}/g,
-      interpolate: /\{\{([\s\S]+?)\}\}/g,
-      escape: /\{\{\{([\s\S]+?)\}\}\}/g,
-      variable: 'ctx'
-    }))
-    .pipe(insert.prepend('Object.defineProperty(exports, "__esModule", {\n' +
-      '  value: true\n' +
-      '});\n' +
-      'exports.default='))
-    .pipe(rename({
-      extname: '.ejs.js'
-    }))
-    .pipe(gulp.dest('lib'))
+  gulp
+    .src('./src/**/*.ejs')
+    .pipe(
+      template.precompile({
+        evaluate: /\{%([\s\S]+?)%\}/g,
+        interpolate: /\{\{([\s\S]+?)\}\}/g,
+        escape: /\{\{\{([\s\S]+?)\}\}\}/g,
+        variable: 'ctx',
+      }),
+    )
+    .pipe(
+      insert.prepend(
+        'Object.defineProperty(exports, "__esModule", {\n' +
+          '  value: true\n' +
+          '});\n' +
+          'exports.default=',
+      ),
+    )
+    .pipe(
+      rename({
+        extname: '.ejs.js',
+      }),
+    )
+    .pipe(gulp.dest('lib')),
 );
 
-gulp.task('css', function(){
-  return gulp.src('src/sass/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('lib/css'))
+gulp.task('css', function () {
+  return gulp.src('src/sass/*.scss').pipe(sass()).pipe(gulp.dest('lib/css'));
 });
 
-gulp.task('sass', function(){
-  return gulp.src('src/sass/*.scss')
-    .pipe(gulp.dest('lib/sass'))
+gulp.task('sass', function () {
+  return gulp.src('src/sass/*.scss').pipe(gulp.dest('lib/sass'));
 });
 
-gulp.task('copy-css', function(){
-  return gulp.src('lib/css/styles.css')
-    .pipe(rename('uswds.css'))
-    .pipe(gulp.dest('dist'))
+gulp.task('copy-css', function () {
+  return gulp.src('lib/css/styles.css').pipe(rename('uswds.css')).pipe(gulp.dest('dist'));
 });
 
 gulp.task('minify-css', () => {
-  return gulp.src('dist/uswds.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(rename({
-      extname: '.min.css'
-    }))
+  return gulp
+    .src('dist/uswds.css')
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(
+      rename({
+        extname: '.min.css',
+      }),
+    )
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build', gulp.series(
-  'clean',
-  gulp.parallel(
-    'templates',
-    'css',
-    'sass'
-  ),
-  'copy-css',
-  'minify-css'
-));
+gulp.task(
+  'build',
+  gulp.series('clean', gulp.parallel('templates', 'css', 'sass'), 'copy-css', 'minify-css'),
+);
